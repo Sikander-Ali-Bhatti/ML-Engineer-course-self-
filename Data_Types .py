8cd601d099901d47c68e131dbf930dbb118afd96
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2025-12-10 21:28:21 +0500

    Add files via upload

---
+++ Data_Types .py
@@ -0,0 +1,37 @@
+#Mutable Data Types:=. change able 
+##list ,  dictornary ,byte Array
+#Imutabable data types:=> unchange able
+#int ,float , complex , string  , tuple  ,set
+
+#Number
+#integers
+#float
+#complex number
+A=23
+b=44
+c=34
+z=A+b+c
+print(type(A),A)
+print (type(b),b)
+print(type(c),c)
+print (type(z),"the sum is z=",z)
+name="Sikander Ali"
+cont="03070188899"
+print (type(cont),"contact number is" ,cont)#str type
+sur_name="bhatti"
+print(name,sur_name)
+#int only store in int value 43
+#float only decimal value store 23.2322
+#str only store string character start with (" ")cotation 
+#boolean only two value True and False
+#list we can use iteam store square bracket["mango","apple",..."N"]
+# tuple as a list  but start with round barcket (10,10)
+#dictionary we can store in data key-value start with curly  barcket{}
+#set iteam collection maen No repeated value allowed
+x=34.4
+y=True
+list=["apple","banana ,garphs...."]
+tuple=(10,34,4)
+print (type(x),x)
+print(type(y),y)
+print((type),list)
