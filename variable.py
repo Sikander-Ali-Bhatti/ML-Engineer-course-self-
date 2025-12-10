0c2c9656a2190a78658710ae6b70618377140f3b
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2025-12-10 16:15:10 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,13 @@
+#create variable time 
+#no start  with number(1,2,3...etc)
+# No Space 
+# use with underscore(example_1)
+a=17
+b=14
+print(a,b)
+print(id(a),id(b))# show memory address/location
+# variable is container  where we store a value if we need value print then we cell only variable 
+#print ("a") not a variable this is a string variable
+Name="Sikander Ali"
+print(type(Name)) # data types show
+ 
