130d6e405ab3d31d067c8986acfb9e271388c410
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2025-12-11 19:02:51 +0500

    Add files via upload

---
+++ student_grade.py
@@ -0,0 +1,16 @@
+score=int(input("enter a no:"))
+if score>=80:
+    print("you got on A")
+    
+if score>=70:
+    print("you got On B")
+    
+if score>=50:
+    print("you  got on C")
+    
+if score>=50:
+    print("you just pass ")
+    
+else:
+    print("once again try")        
+    
