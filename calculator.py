9c765ccecaf80b9badc3a12a98d1f7a3dd889588
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2025-12-11 18:42:29 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+A=int(input("enetr a  number 1:"))
+b=int(input("enetr a  number 2:"))
+opr=input("oprator:")
+
+if opr=="+":
+    print(" Addition of A and B",A+b)
+if opr=="-":
+    print("subtract of A and B",A-b)
+if opr=="/": 
+    print("divide of A and B",A/b)
+if opr=="*":
+    print("multiple of A and B",A*b)
+    
+else:
+    print("invalid opr")                
+     
