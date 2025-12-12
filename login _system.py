5d4b27a00491249b47db00b5544525fb4f96632c
Sikander-Ali-Bhatti <152159856+Sikander-Ali-Bhatti@users.noreply.github.com>
2025-12-12 20:01:17 +0500

    Add files via upload

---
+++ login _system.py
@@ -0,0 +1,8 @@
+correct_username="sikander"
+correct_userpassword="088899"
+name=input("enter a your name:")
+password=input("enter a password:")
+if name==correct_username and password==correct_userpassword:
+    print("your page is login")
+else:
+    print("user not in register") 
